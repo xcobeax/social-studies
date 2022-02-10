75164d8a582815f4c7868484e0f63f0ce698a393
arie-azh-ari <Arie.Azhari@medirecords.com>
2022-02-11 00:21:20 +0700

    LFS

--- src/Data/Indonesia/index.js
+++
@@ -1,9 +0,0 @@
-import dataJson from './indonesia.json'
-export const data = dataJson.parent
-export const getDetail = (id) => new Promise((resolve, reject) => {
-    const resp = dataJson.children
-    if (resp) {
-        resolve(resp.filter(e => e.groupId === id))
-    }
-    reject("Error")
-})
